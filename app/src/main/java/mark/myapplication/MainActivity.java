818e8c161769c15dcf368b1f64b97822dd07e632
Nguyen <hdnf39@kc-sce-18wg8wn.kc.umkc.edu>
2016-11-01 13:58:57 -0500

    Initial commit

---
+++ app/src/main/java/mark/myapplication/MainActivity.java
@@ -0,0 +1,302 @@
+package mark.myapplication;
+
+import android.content.Context;
+import android.net.Uri;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView InfoText;
+    Button changeIDButton;
+    Button sendDataButton;
+    Button getInfo;
+
+    EditText textOut;
+    TextView textIn;
+
+    WifiConfiguration wifiConf;
+
+    int count;
+    WifiManager manager;
+
+    private static Context context;
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        MainActivity.context = getApplicationContext();
+
+        InfoText = (TextView) findViewById(R.id.InfoText);
+        changeIDButton = (Button) findViewById(R.id.button);
+        changeIDButton.setOnClickListener(buttonChangeOnClickListener);
+        sendDataButton = (Button) findViewById(R.id.button2);
+        sendDataButton.setOnClickListener(buttonSendOnClickListener);
+        getInfo = (Button) findViewById(R.id.button3);
+        getInfo.setOnClickListener(buttonGetInfoOnClickListener);
+
+        textOut = (EditText) findViewById(R.id.textout);
+
+
+        manager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+
+
+        count = 0;
+
+        wifiConf = null;
+        WifiManager wifiManager = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+        WifiInfo connectionInfo = wifiManager.getConnectionInfo();
+        List<WifiConfiguration> configuredNetworks = wifiManager.getConfiguredNetworks();
+        for (WifiConfiguration conf : configuredNetworks){
+            if (conf.networkId == connectionInfo.getNetworkId()){
+                wifiConf = conf;
+                break;
+            }
+        }
+        //getID();
+
+        // Example of a call to a native method
+        //TextView tv = (TextView) findViewById(R.id.sample_text);
+        //tv.setText(stringFromJNI());
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    public void getID() {
+        String macVlan = Utils.getMACAddress("wlan0");
+        String macEth = Utils.getMACAddress("eth0");
+        String ipV4 = Utils.getIPAddress(true);
+        String ipV6 = Utils.getIPAddress(false);
+
+        count++;
+
+        InfoText.setText("ipv4: " + ipV4 + "\n"
+                + "ipv6: " + ipV6 + "\n"
+                + "macVlan: " + macVlan + "\n"
+                + "macEth: " + macEth + "\n"
+                + "count: " + count
+        );
+
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native void changeIP();
+
+    Button.OnClickListener buttonChangeOnClickListener
+            = new Button.OnClickListener() {
+        @Override
+        public void onClick(View arg0) {
+            //changeIP();
+            try {
+                setStaticIpConfiguration(manager, wifiConf,
+                        InetAddress.getByName("10.0.0.1"), 24,
+                        InetAddress.getByName("10.0.0.2"),
+                        new InetAddress[] { InetAddress.getByName("10.0.0.3"), InetAddress.getByName("10.0.0.4") });            }catch (Exception e){
+                System.out.println("Exception");
+            }
+        }
+    };
+
+    Button.OnClickListener buttonGetInfoOnClickListener
+            = new Button.OnClickListener() {
+        @Override
+        public void onClick(View arg0) {
+            getID();
+        }
+    };
+
+
+    Button.OnClickListener buttonSendOnClickListener
+            = new Button.OnClickListener() {
+
+        @Override
+        public void onClick(View arg0) {
+            // TODO Auto-generated method stub
+            Socket socket = null;
+            DataOutputStream dataOutputStream = null;
+            DataInputStream dataInputStream = null;
+
+            try {
+                socket = new Socket("10.205.0.136", 8888);
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                dataOutputStream.writeUTF(textOut.getText().toString());
+                textIn.setText(dataInputStream.readUTF());
+            } catch (UnknownHostException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+                if (dataOutputStream != null) {
+                    try {
+                        dataOutputStream.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+                if (dataInputStream != null) {
+                    try {
+                        dataInputStream.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    };
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
+    @SuppressWarnings("unchecked")
+    private static void setStaticIpConfiguration(WifiManager manager, WifiConfiguration config, InetAddress ipAddress, int prefixLength, InetAddress gateway, InetAddress[] dns) throws ClassNotFoundException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, NoSuchFieldException, InstantiationException
+    {
+        // First set up IpAssignment to STATIC.
+        Object ipAssignment = getEnumValue("android.net.IpConfiguration$IpAssignment", "STATIC");
+        callMethod(config, "setIpAssignment", new String[] { "android.net.IpConfiguration$IpAssignment" }, new Object[] { ipAssignment });
+
+        // Then set properties in StaticIpConfiguration.
+        Object staticIpConfig = newInstance("android.net.StaticIpConfiguration");
+        Object linkAddress = newInstance("android.net.LinkAddress", new Class<?>[] { InetAddress.class, int.class }, new Object[] { ipAddress, prefixLength });
+
+        setField(staticIpConfig, "ipAddress", linkAddress);
+        setField(staticIpConfig, "gateway", gateway);
+        getField(staticIpConfig, "dnsServers", ArrayList.class).clear();
+        for (int i = 0; i < dns.length; i++)
+            getField(staticIpConfig, "dnsServers", ArrayList.class).add(dns[i]);
+
+        callMethod(config, "setStaticIpConfiguration", new String[] { "android.net.StaticIpConfiguration" }, new Object[] { staticIpConfig });
+        manager.updateNetwork(config);
+        manager.saveConfiguration();
+    }
+
+    private static Object newInstance(String className) throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, IllegalArgumentException, InvocationTargetException
+    {
+        return newInstance(className, new Class<?>[0], new Object[0]);
+    }
+
+    private static Object newInstance(String className, Class<?>[] parameterClasses, Object[] parameterValues) throws NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, ClassNotFoundException
+    {
+        Class<?> clz = Class.forName(className);
+        Constructor<?> constructor = clz.getConstructor(parameterClasses);
+        return constructor.newInstance(parameterValues);
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    private static Object getEnumValue(String enumClassName, String enumValue) throws ClassNotFoundException
+    {
+        Class<Enum> enumClz = (Class<Enum>)Class.forName(enumClassName);
+        return Enum.valueOf(enumClz, enumValue);
+    }
+
+    private static void setField(Object object, String fieldName, Object value) throws IllegalAccessException, IllegalArgumentException, NoSuchFieldException
+    {
+        Field field = object.getClass().getDeclaredField(fieldName);
+        field.set(object, value);
+    }
+
+    private static <T> T getField(Object object, String fieldName, Class<T> type) throws IllegalAccessException, IllegalArgumentException, NoSuchFieldException
+    {
+        Field field = object.getClass().getDeclaredField(fieldName);
+        return type.cast(field.get(object));
+    }
+
+    private static void callMethod(Object object, String methodName, String[] parameterTypes, Object[] parameterValues) throws ClassNotFoundException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException
+    {
+        Class<?>[] parameterClasses = new Class<?>[parameterTypes.length];
+        for (int i = 0; i < parameterTypes.length; i++)
+            parameterClasses[i] = Class.forName(parameterTypes[i]);
+
+        Method method = object.getClass().getDeclaredMethod(methodName, parameterClasses);
+        method.invoke(object, parameterValues);
+    }
+}
